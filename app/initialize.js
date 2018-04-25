2e17a551e9b2b9616444d576c8c495802ee972a1
Francis Cabral <externe.francis.cabral@allianz.fr>
2018-04-25 17:49:05 +0200

    Adding bootstrap 4 : not finalize

--- app/initialize.js
+++ app/initialize.js
@@ -1,18 +1,41 @@
-import $ from 'jquery';
-import Rx from 'rxjs/Rx';
+import $ from "jquery";
+import Rx from "rxjs/Rx";
+import 'bootstrap';
 
 //var $ = require('jquery');
 //var Rx = require('rxjs/Rx');
 
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // do your setup here
-  console.log('Initialized app');
-  console.log('Tasty Brunch, just trying to use jQuery!', $('body'));
-  
-  
+  console.log("Initialized app");
+  console.log("Tasty Brunch, just trying to use jQuery!", $("body"));
+
+  //document.querySelector('rxjs').innerHTML('Approuved');
+  // $("#rxjs").html ('Approuved');
+
+  //const source$  = Rx.Observable.interval(1000).take(5);
+  //source$.subscribe(v => console.log('mytestyXXX   '+v));
+
+  const posts = [
+    { title: "Post One", body: "My body" },
+    { title: "Post Two", body: "My body" },
+    { title: "Post Three", body: "My body" }
+  ];
+
+  const postOutput = $("#posts");
+
+  const posts$ = Rx.Observable.from(posts);
+
+  posts$.subscribe(
+    post => {
+      console.log(post.title);
+      $("#posts").append('<li>'+post.title+'</li>');
+    },
+    err => {
+      console.error(err);
+    },
+    complete => {
+      console.log("Completed");
+    }
+  );
 });
-
-const source$  = Rx.Observable.interval(1000).take(5);
-
-  source$.subscribe(v => console.log('mytestyXXX   '+v));
